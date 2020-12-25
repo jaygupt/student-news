3e0e8c55d21b10711dafe71d01af647c0b95d8d4
KaUsHik-HaRiHaRaN <hkaushik58@gmail.com>
2020-12-24 16:15:24 -0800

    created preliminary cardview setup

---
+++ src/Components/Cardview.js
@@ -0,0 +1,41 @@
+import React, {useEffect} from 'react';
+import './Cardview.css';
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+
+const Cardview = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000});
+    }, []);
+    return(
+        <div className = "cardList">
+            <div className = "main-cards" data-aos="zoom-in" data-aos-duration="1000">
+                <h3><center>This is a card</center></h3>
+            </div>
+
+            <div className = "main-cards" data-aos="zoom-in" data-aos-duration="1000">
+                <h3><center>This is a card</center></h3>
+            </div>
+
+            <div className = "main-cards" data-aos="zoom-in" data-aos-duration="1000">
+                <h3><center>This is a card</center></h3>
+            </div>
+
+            <div className = "main-cards" data-aos="zoom-in" data-aos-duration="1000">
+                <h3><center>This is a card</center></h3>
+            </div>
+
+            <div className = "main-cards" data-aos="zoom-in" data-aos-duration="1000">
+                <h3><center>This is a card</center></h3>
+            </div>
+
+            <div className = "main-cards" data-aos="zoom-in" data-aos-duration="1000">
+                <h3><center>This is a card</center></h3>
+            </div>
+        </div> 
+    );
+}
+
+
+export default Cardview;
