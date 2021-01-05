7d0420e03f4e943961b445cee3da55fa2eb848d6
Jay Gupta <jaygupt1973@gmail.com>
2021-01-04 18:46:35 -0800

    Add horizontal line

--- src/Components/Attributes.js
+++ src/Components/Attributes.js
@@ -5,28 +5,31 @@ import "./Attributes.css"
 
 function Attributes() {
   return (
-    <ul className="card-attributes">
-      <li><AppButton 
-        type="Like" 
-        icon={<ThumbUp fontSize="medium"/>}
-        buttonColor="#1DA1F2"
-      /></li>
-      <li><AppButton 
-        type="Save" 
-        icon={<Save fontSize="medium"/>}
-        buttonColor="#DFE1E4"
-      /></li>
-      <li><AppButton 
-        type="Reply" 
-        icon={<Reply fontSize="medium"/>}
-        buttonColor="#5DCD11"
-      /></li>
-      <li><AppButton 
-        type="Share" 
-        icon={<Share fontSize="medium"/>}
-        buttonColor="#FF4477"  
-      /></li>
-    </ul>
+    <div className="main-div">
+      <hr className="card-line"/>
+      <ul className="card-attributes">
+        <li><AppButton 
+          type="Like" 
+          icon={<ThumbUp fontSize="medium"/>}
+          buttonColor="#1DA1F2"
+        /></li>
+        <li><AppButton 
+          type="Save" 
+          icon={<Save fontSize="medium"/>}
+          buttonColor="#DFE1E4"
+        /></li>
+        <li><AppButton 
+          type="Reply" 
+          icon={<Reply fontSize="medium"/>}
+          buttonColor="#5DCD11"
+        /></li>
+        <li><AppButton 
+          type="Share" 
+          icon={<Share fontSize="medium"/>}
+          buttonColor="#FF4477"  
+        /></li>
+      </ul>
+    </div>
   ); 
 }
 
