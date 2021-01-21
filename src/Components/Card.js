8cb70fc629b100a761c663391676ee6733846e4d
Jay Gupta <jaygupt1973@gmail.com>
2021-01-20 16:11:34 -0800

    Modify card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,10 +2,16 @@ import React from "react";
 import './Card.css';
 import Attributes from "./Attributes"; 
 
-function Card() {
+function Card(props) {
   return (
-    <div className = "main-cards" data-aos="zoom-in" data-aos-duration="1000">
-      <h3 className = "cardtext"><center>This is a card</center></h3>
+    <div className="main-cards" data-aos="zoom-in" data-aos-duration="1000">
+      <div className="cardElements">
+        <img src={props.imageSrc} width="160px" height="160px" alt={props.imageAlt} className="cardImage"/>
+        <div className="cardText">
+          <h3 className="cardTitle">{props.cardTitle}</h3>
+          <p className="cardSummary">{props.cardSummary.substring(0, 190)}...</p>
+        </div>
+      </div>
       <Attributes />
     </div>
   ); 
--- src/Components/Cardview.js
+++ src/Components/Cardview.js
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react';
 import Card from "./Card"; 
-// import './Cardview.css';
 import Aos from "aos";
 import "aos/dist/aos.css";
 
@@ -10,9 +9,24 @@ const Cardview = () => {
     }, []);
     return(
         <div className = "cardList">
-            <Card />
-            <Card />
-            <Card />
+            <Card 
+                imageSrc="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+                imageAlt="Sample Image"
+                cardTitle="Hello World"
+                cardSummary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu sem aliquam, tincidunt eros vitae, tempor felis. Maecenas suscipit ex eu leo ornare, eget sollicitudin orci eleifend hell"
+            />
+            <Card 
+                imageSrc="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+                imageAlt="Sample Image"
+                cardTitle="Hello World"
+                cardSummary="Hello, everyone! This is the LONGEST TEXT EVER! I was inspired by the various other longest texts ever on the internet, and I wanted to make my own. So here it is! This is going to be a WORLD RECORD! This is actually my third attempt at doing this. The first time, I didn't save it. The second time, the Neocities editor crashed. Now I'm writing this in Notepad, then copying it into"
+            />
+            <Card 
+                imageSrc="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+                imageAlt="Sample Image"
+                cardTitle="Hello World"
+                cardSummary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu sem aliquam, tincidunt eros vitae, tempor felis. Maecenas suscipit ex eu leo ornare, eget sollicitudin orci eleifend"
+            />
         </div> 
     );
 }
